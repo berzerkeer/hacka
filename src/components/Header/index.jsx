cab49ec5a869c2262d8272ec9393a1f47e3b5b20
Sangeeth Sivan <sangeethsivan036@gmail.com>
2022-02-18 16:21:47 +0530

    ✨ (feat) : header component with user chip and app title

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import UserChip from 'components/UserChip';
+import React from 'react';
+import './header.scss';
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="container flex flex-jc-sb flex-ai-c">
+        <a href="/" className="header_logo">
+          <h2>berzerk.</h2>
+        </a>
+        <ul className="header__menu flex flex-ai-c">
+          <span className="header__menu__profiletag">
+            <UserChip name="Sangeeth" />
+          </span>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
