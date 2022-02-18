83dc59ff97d6e8b55e095699ab1f5dbdc2603ce7
Sangeeth Sivan <sangeethsivan036@gmail.com>
2022-02-18 22:29:15 +0530

    💄 (UI) : updated card ui to show user info and vote count

--- src/components/HackathonCard/index.jsx
+++ src/components/HackathonCard/index.jsx
@@ -1,7 +1,11 @@
 import UserChip from 'components/UserChip';
 import React from 'react';
 
-// import { ReactComponent as ArrowRight } from 'assets/icons/arrow-right.svg';
+import IconBadge from 'components/IconBadge';
+
+import { ReactComponent as ThumbsUp } from 'assets/icons/thumbs-up.svg';
+import { ReactComponent as ArrowUp } from 'assets/icons/arrow-up.svg';
+
 import './hackathoncard.scss';
 
 function HackathonCard({ testText }) {
@@ -11,20 +15,41 @@ function HackathonCard({ testText }) {
 
   return (
     <div className="card flex">
-      <div className="card__content flex flex-ai-c">
-        <div className="card__meta flex flex-ai-c">
-          <UserChip />
+      <div className="card__content flex ">
+        <div className="card__meta ">
+          <IconBadge label="46">
+            <ArrowUp />
+          </IconBadge>
         </div>
         <div className="card__details">
-          <h2 className="hackathon_title">Lowcode tool for Payments</h2>
-          <p className="hackathon_description">
-            Lowcode no code is booming and i believe a low code platform to
-            build payments integrations is a greay way to go about.{' '}
-          </p>
-          <button className="hackathon_details" onClick={handleClick}>
-            <span>More details </span>
-            <span>{/* <ArrowRight /> */}</span>
-          </button>
+          <div className="hackathon__info">
+            <h2 className="hackathon__info__title">
+              Lowcode tool for Payments
+            </h2>
+            <p className="hackathon__info__description">
+              Lowcode no code is booming and i believe a low code platform to
+              build payments integrations is a greay way to go about. Lowcode no
+              code is booming and i believe a low code platform to build
+              payments integrations is a greay way to go about. Lowcode no code
+              is booming and i believe a low code platform to build payments
+              integrations is a greay way to go about. Lowcode no code is
+              booming and i believe a low code platform to build payments
+              integrations is a greay way to go about.
+            </p>
+          </div>
+          <div className="user__info flex flex-ai-c">
+            <div className="user__info__meta flex flex-ai-c">
+              <span className="chip">
+                <UserChip />
+              </span>
+              <span className="annotation">
+                posted by <span className="name">Sangeeth Sivan</span>
+              </span>
+            </div>
+            <div className="user__info__post">
+              <span className="date">12 hr ago</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
