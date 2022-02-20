743ae70d9087970a133075579301dc2c03d68a2b
Sangeeth Sivan <sangeethsivan036@gmail.com>
2022-02-20 22:32:18 +0530

    ✨ (feat) : Login is now functional

--- src/components/Forms/Login/index.jsx
+++ src/components/Forms/Login/index.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useUserStore } from 'store/user';
+
 import './login.scss';
 
 function LoginForm() {
-  function handleSubmit(e) {
+  const [empId, setEmpId] = useState('');
+  const history = useHistory();
+  const handleInput = (e) => {
     e.preventDefault();
-    console.log('logged in');
+    setEmpId(e.target.value);
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await fetch('http://localhost:8000/user');
+      const resJson = await res.json();
+      const user = resJson.find((emp) => emp.empId === empId);
+      if (user) {
+        history.push('/all');
+        useUserStore.getState().setIsLoggedIn(true);
+        useUserStore.getState().setCurrentUser(user);
+      } else {
+        console.log('No user found');
+      }
+    } catch (error) {
+      console.error('Error fetching user data for login', error);
+    }
   }
+
+  const handleClick = () => {
+    history.push('/signup');
+  };
 
   return (
     <form onSubmit={handleSubmit} className="form flex flex-ai-c flex-jc-c">
       <div className="form__group flex flex-jc-c">
         <input
-          onChange={(e) => {
-            console.log(e.target.value);
-          }}
+          onChange={handleInput}
           id="emp-login"
           name="empId"
           className="form__control"
@@ -25,6 +50,9 @@ function LoginForm() {
           Login
         </button>
       </div>
+      <span className="create__account" onClick={handleClick}>
+        Create an account
+      </span>
     </form>
   );
 }
