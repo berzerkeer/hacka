376ac5b4955b9a167a734ea7803861b0d45ced2f
Sangeeth Sivan <sangeethsivan036@gmail.com>
2022-02-20 22:40:10 +0530

    ✨ (feat) : display posts from store

--- src/components/StackLayout/index.jsx
+++ src/components/StackLayout/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import HackathonCard from 'components/HackathonCard';
@@ -7,13 +7,33 @@ import AddNewModal from 'components/Modal/AddNewModal';
 import './stacklayout.scss';
 
 function StackLayout() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const res = await fetch('http://localhost:8000/post');
+        const data = await res.json();
+        setPosts(data);
+      } catch (error) {
+        console.error('Error fetching posts', error);
+      }
+    }
+    fetchPosts();
+    return () => {
+      console.log('later utilize abort controller');
+    };
+  }, []);
+
   const { pageId } = useParams();
   return (
     <>
       <h2 className="page__title">{pageId}</h2>
       <AddNewModal />
       <div className="gridparent">
-        <HackathonCard />
+        {posts.map((post) => (
+          <HackathonCard key={post.id} post={post} />
+        ))}
       </div>
     </>
   );
