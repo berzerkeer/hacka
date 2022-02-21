85af038c193db084fb26db461691acd1b78f37fd
Sangeeth Sivan <sangeethsivan036@gmail.com>
2022-02-21 18:18:07 +0530

    ✨ (feat) : upvote state transition for icon badge

--- src/components/IconBadge/index.jsx
+++ src/components/IconBadge/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './iconbadge.scss';
 
-function IconBadge({ children, label }) {
+function IconBadge({ children, label, handleClick, isAlreadyUpVoted }) {
+  const onUpvote = (e) => {
+    e.preventDefault();
+    handleClick();
+  };
+
   return (
-    <div className="badge__container flex flex-ai-c">
+    <div
+      className={`badge__container flex flex-ai-c ${
+        isAlreadyUpVoted ? 'upvoted' : ''
+      }`}
+      onClick={onUpvote}
+    >
       <span className="badge__container__icon">{children}</span>
       <span className="badge__container__label">{label}</span>
     </div>
