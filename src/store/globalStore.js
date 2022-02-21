d798a6aa94373cc552d3ccf7ed8b50bc7af9dd5d
Sangeeth Sivan <sangeethsivan036@gmail.com>
2022-02-21 18:24:46 +0530

    ✨ (feat) : added global store to track users,posts lists

--- src/store/globalStore.js
+++ src/store/globalStore.js
@@ -2,20 +2,61 @@ import create from 'zustand';
 import { devtools } from 'zustand/middleware';
 
 const appInitialState = {
-  posts: []
+  posts: [],
+  users: [],
+  currentPostId: ''
 };
 
 export const useAppstore = create(
   devtools((set, get) => ({
     ...appInitialState,
-    setPosts: (posts) => {
+    getPosts: async function getPosts() {
+      try {
+        const res = await fetch('http://localhost:8000/post');
+        const data = await res.json();
+        set(() => ({
+          posts: data
+        }));
+      } catch (error) {
+        console.error('Error fetching posts', error);
+      }
+    },
+    getUsers: async function getUsers() {
+      try {
+        const res = await fetch('http://localhost:8000/user');
+        const data = await res.json();
+        set(() => ({
+          users: data
+        }));
+      } catch (error) {
+        console.error('Error fetching posts', error);
+      }
+    },
+    setPostsbySortOrder: (posts) => {
       set(() => ({
         posts
       }));
     },
-    updatePosts: (posts) => {
+    setPosts: (posts) => {
       set((state) => ({
         posts: [...state.posts, posts]
+      }));
+    },
+    updatePosts: (post) => {
+      set((state) => ({
+        posts:
+          state.posts &&
+          state.posts.map((obj) => {
+            if (obj.id === post.id) {
+              return { ...obj, ...post };
+            }
+            return obj;
+          })
+      }));
+    },
+    setCurrentPostId: (postId) => {
+      set(() => ({
+        currentPostId: postId
       }));
     }
   }))
